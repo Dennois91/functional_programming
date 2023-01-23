d9e56e3278692c2980dc5d60e9a36b78fdf96b6a
Dennois91 <109462612+Dennois91@users.noreply.github.com>
2023-01-23 12:22:16 +0100

    Connection established

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,47 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.*;
+import java.util.Properties;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    Properties p = new Properties();
+
+
+    public Main() {
+
+
+        loadProperties();
+        connectToAndQueryDatabase(p.getProperty("username"), p.getProperty("password"));
+
+
     }
+
+    private void loadProperties() {
+        try {
+            p.load(new FileInputStream("resources/settings.properties"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void connectToAndQueryDatabase(String username, String password) {
+        try (Connection con = DriverManager.getConnection(p.getProperty("connectionString"), username, password)) {
+            // Statement stmt = con.createStatement();
+            // ResultSet rs = stmt.executeQuery("SELECT a,b,c FROM TABLE1");
+
+            //  while (rs.next()){
+            //      int x = rs.getInt("a");
+            //     String s = rs.getString("b");
+            //       float f = rs.getFloat("c");
+            //  }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        new Main();
+    }
+
 }

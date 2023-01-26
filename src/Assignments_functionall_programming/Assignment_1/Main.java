2ab0181bf12558780e0eba1695341c730b2bc1b6
Dennois91 <109462612+Dennois91@users.noreply.github.com>
2023-01-26 18:22:54 +0100

    assignment 2 and 1 Funktionell programming

--- src/Assignments_functionall_programming/Assignment_1/Main.java
+++ src/Assignments_functionall_programming/Assignment_1/Main.java
@@ -1,33 +1,37 @@
 package Assignments_functionall_programming.Assignment_1;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public class Main {
-private Util util = new Util();
+    private final Util util = new Util();
+    List<Book> books = util.getBooks();
+
     public Main() {
-        System.out.println("hello");
+        util.createBooks();
+
+        util.getAllBooksByAuthor("J.R.R. Tolkien", books).forEach(book -> System.out.println(book.getTitel()));
+        util.getAllBooksByColour("Yellow", books).forEach(book -> System.out.println(book.getTitel()));
+        System.out.println(util.countBooksByOwner("Brian Wilson",books));
+
+        System.out.println(util.getAllTitelsOfBooks(books));
+        System.out.println(util.getAllAuthors(books));
+        System.out.println(util.getAllBookTitelsOwnedBy(books,"Brian Wilson"));
+
 
     }
 
     public static void main(String[] args) {
+        new Main();
 
     }
 }
 /*
-Skapa en klass Book, som minst innehåller följande medlemsvariabler (med getters och setters):
-• Titel
-• Författare
-• Genre
-• Färg
-• Betyg (int)
-• Ägare
-• Fakta eller fiction (boolean)
-Skapa en Util-klass där du skapar upp ett gäng böcker och lägger dessa i en lista. Skapa sedan funktion
-getList där du returnerar din boklista. Se till att ha några böcker med samma författare, några med
-samma färg, olika betyg och olika ägare till böckerna i listan.
 Skriv följande funktioner som alla ska använda lambda-metoden filter:
 • En funktion som söker ut alla böcker av en viss författare
 • En funktion som söker ut alla böcker av en viss färg
 • En funktion som räknar antal böcker som tillhör dig
-
 
 Uppgift 1b– Basic strömmar, map
 Jobba vidare med din bok-lista.
@@ -36,5 +40,4 @@ Skriv följande funktioner som alla ska använda lambda-metoden map:
 • En funktion som returnerar en lista på alla författare som dina böcker har
 o Vi vill inte ha några dubletter i listan
 • En funktion som listar alla titlar, men bara på böcker som tillhör dig
-
  */
--- src/Assignments_functionall_programming/Assignment_1/Util.java
+++ src/Assignments_functionall_programming/Assignment_1/Util.java
@@ -2,17 +2,18 @@ package Assignments_functionall_programming.Assignment_1;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Util {
     List<Book> books = new ArrayList<>();
 
     public void createBooks() {
-        books.add(new Book("LOTRO", "Tolkien", "Fantasy", "Green", 5, "Brian Wilson", false));
-        books.add(new Book("Harry Potter,Chamber of secrets", "J.K.Rowling", "Fantasy", "Red", 4, "Brian Wilson", false));
-        books.add(new Book("The Great Gatsby", "F. Scott Fitzgerald", "Novel", "Green", 5, "John Doe", true));
+        books.add(new Book("LOTRO", "J.R.R. Tolkien", "Fantasy", "Green", 5, "Brian Wilson", false));
+        books.add(new Book("Harry Potter,Chamber of secrets", "J.K. Rowling", "Fantasy", "Red", 4, "Brian Wilson", false));
+        books.add(new Book("The Great Gatsby", "F. Scott Fitzgerald", "Novel", "Green", 5, "John Doe", false));
         books.add(new Book("Harry Potter and the Sorcerer's Stone", "J.K. Rowling", "Fantasy", "Yellow", 4, "Jane Smith", false));
-        books.add(new Book("To Kill a Mockingbird", "Harper Lee", "Novel", "Blue", 5, "Bob Johnson", true));
-        books.add(new Book("The Catcher in the Rye", "J.D. Salinger", "Novel", "Orange", 3, "Mary Williams", true));
+        books.add(new Book("To Kill a Mockingbird", "Harper Lee", "Novel", "Blue", 5, "Bob Johnson", false));
+        books.add(new Book("The Catcher in the Rye", "J.D. Salinger", "Novel", "Orange", 3, "Mary Williams", false));
         books.add(new Book("The Hobbit", "J.R.R. Tolkien", "Fantasy", "Red", 4, "Mike Brown", false));
         books.add(new Book("The Alchemist", "Paulo Coelho", "Fiction", "Gold", 5, "Emily Davis", false));
         books.add(new Book("The Girl on the Train", "Paula Hawkins", "Mystery", "Black", 4, "Daniel Rodriguez", true));
@@ -20,6 +21,37 @@ public class Util {
         books.add(new Book("The Divine Comedy", "Dante Alighieri", "Poetry", "White", 3, "Brian Wilson", true));
         books.add(new Book("The Picture of Dorian Gray", "Oscar Wilde", "Novel", "Silver", 4, "Emily Johnson", true));
     }
+// En funktion som returnerar en lista på alla titlar som dina böcker har
+    //En funktion som returnerar en lista på alla författare som dina böcker har
+    //o Vi vill inte ha några dubletter i listan
+    //En funktion som listar alla titlar, men bara på böcker som tillhör dig
+
+    public List<String> getAllBookTitelsOwnedBy(List<Book> list, String owner){
+        return list.stream().filter(Book -> Book.getOwner().equalsIgnoreCase(owner)).
+                map(Book::getTitel).collect(Collectors.toList());
+    }
+    public List<String> getAllAuthors(List<Book> list) {
+        return list.stream().map(Book::getAuthor).distinct().toList();
+    }
+
+    public List<String> getAllTitelsOfBooks(List<Book> list) {
+        return list.stream().map(Book::getTitel).toList();
+    }
+
+    public long countBooksByOwner(String owner, List<Book> list) {
+        return list.stream().filter(s -> s.getOwner().equalsIgnoreCase(owner)).count();
+    }
+
+    public List<Book> getAllBooksByColour(String colour, List<Book> list) {
+        return list.stream().filter(s -> s.getColour().equalsIgnoreCase(colour))
+                .toList();
+    }
+
+    public List<Book> getAllBooksByAuthor(String author, List<Book> list) {
+        return list.stream().filter(s -> s.getAuthor().equalsIgnoreCase(author))
+                .toList();
+    }
+
 
     public List<Book> getBooks() {
         return books;

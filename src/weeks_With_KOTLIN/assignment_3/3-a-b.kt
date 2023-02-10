b24065dcfee8aca36ab74d47569ff283ed3552a7
Dennois91 <109462612+Dennois91@users.noreply.github.com>
2023-02-10 14:01:08 +0100

    Started with Kotlin

--- src/weeks_With_KOTLIN/assignment_3/3-a-b.kt
+++ src/weeks_With_KOTLIN/assignment_3/3-a-b.kt
@@ -1,5 +1,73 @@
 package weeks_With_KOTLIN.assignment_3
 
+import java.lang.Exception
+import java.lang.NumberFormatException
+
+var loop: Boolean = true
+var generatedNumber = generateRandom()
+fun main() {
+
+    while (loop) {
+        guessNumber(generatedNumber)
+    }
+}
+
+fun generateRandom() = (1..10).random()
+
+fun guessNumber(random: Int) {
+    println("Guess a number from 1-10")
+    try {
+        val input = readLine()!!.toInt()
+
+        if (input > random)
+            ifToHigh(input)
+        else if (input < random)
+            ifToLow(input)
+        else {
+            ifCorrect(random)
+            loop = askForAnotherGame()
+        }
+
+    } catch (e: NumberFormatException) {
+        println("Not a valid input. use numbers only!")
+
+    } catch (e: Exception) {
+        println("Unexpected error!")
+    }
+}
+
+fun ifToLow(guess: Int) {
+    println(" you guessed $guess but the number is higher then that!")
+}
+
+fun ifToHigh(guess: Int) {
+    println(" you guessed $guess but the number is lower then that!")
+}
+
+fun ifCorrect(guess: Int) {
+    println("you guessed correct! The number was $guess")
+}
+
+fun askForAnotherGame(): Boolean {
+    println("Do you want to play again? enter Y or N")
+    return when (readLine()!!.uppercase()) {
+        "Y" -> {
+            generatedNumber = generateRandom()
+            println("New game started")
+            true
+        }
+
+        "N" -> {
+            println("Good bye")
+            false
+        }
+        else -> {
+            println("Invalid input")
+            askForAnotherGame()
+        }
+    }
+}
+
 /*
 Uppgift 3a - Gissa talet
 Gör ett litet program där du väljer ut ett random värde mellan 1 och 10.
@@ -15,4 +83,3 @@ Kolla att ditt spel funkar fint och att användaren får sitt felmeddelande och 
 NumberFormatException har inträffat
 
 */
-
---
+++ src/weeks_With_KOTLIN/assignment_7/7-a-f.kt
@@ -0,0 +1,92 @@
+package weeks_With_KOTLIN.assignment_7
+
+const val initialValue = 10000.0
+const val yearsToAccumulate = 50
+const val interestRate = 0.07
+const val parenthesis = "(()())())"
+val listOfInts = listOf(1, 2, 3)
+
+fun main() {
+    printInterestOnInterest()
+    println(recursiveSummarize(listOfInts))
+    println(findMaxValueRecursivelyInListOfInts(listOfInts))
+    println(recursivelyReturnAStringReversed("Pele"))
+    println(checkBalanceOfParenthesis(parenthesis))
+    println(fib(1, 2, 30))
+
+}
+
+fun interestOnInterest(initialValue: Double, yearsToAccumulate: Int, interestRate: Double): Double {
+
+    tailrec fun interestAcc(acc: Double, years: Int, interestRate: Double): Double {
+        return if (years == 0) acc
+        else interestAcc(acc * (1 + interestRate), years - 1, interestRate)
+    }
+    return interestAcc(initialValue, yearsToAccumulate, interestRate)
+
+}
+
+fun printInterestOnInterest() {
+    val accumulatedValue =
+        interestOnInterest(initialValue, yearsToAccumulate, interestRate)
+    println("value after $yearsToAccumulate is $accumulatedValue")
+}
+
+fun recursiveSummarize(listOfInts: List<Int>): Int {
+    return when {
+        listOfInts.isEmpty() -> 0
+        else -> listOfInts.first() + recursiveSummarize(listOfInts.drop(1))
+    }
+}
+
+fun findMaxValueRecursivelyInListOfInts(listOfInts: List<Int>): Int {
+
+    tailrec fun findMaxValue(listOfInts: List<Int>, maxValue: Int): Int {
+        return when {
+            listOfInts.isEmpty() -> maxValue
+            else -> {
+                val currentValue = listOfInts[0]
+                val newMaxValue = if (currentValue > maxValue) currentValue else maxValue
+                findMaxValue(listOfInts.drop(1), newMaxValue)
+            }
+        }
+    }
+    return when {
+        listOfInts.isEmpty() -> 0
+        else -> findMaxValue(listOfInts, listOfInts[0])
+    }
+}
+
+fun recursivelyReturnAStringReversed(text: String): String {
+    return when {
+        text.isEmpty() -> ""
+        else -> {
+            recursivelyReturnAStringReversed(text.takeLast(text.length - 1)) + text[0]
+        }
+    }
+}
+
+fun checkBalanceOfParenthesis(text: String): Boolean {
+
+    tailrec fun checkBalanceOfParenthesis(text: String, count: Int): Boolean {
+        if (count < 0) return false
+        if (text.isEmpty()) return count == 0
+        return when (text[0]) {
+            '(' -> checkBalanceOfParenthesis(text.substring(1), count + 1)
+            ')' -> checkBalanceOfParenthesis(text.substring(1), count - 1)
+            else -> checkBalanceOfParenthesis(text.substring(1), count)
+        }
+    }
+    return checkBalanceOfParenthesis(text, 0)
+}
+
+    tailrec fun fib(prevPrev: Int, prev: Int, i : Int) {
+        val next = prevPrev + prev
+        when  {
+            (i == 0) -> println(1)
+            (i == 1) -> println(2)
+            (i-2 == 0) -> println(next)
+        }
+        if (i <= 2) System.exit(0)
+        fib(prev, next, i -1)
+    }

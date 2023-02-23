66768d16dda84917bfc5dc8b6b8fccb4a6de11d9
Dennois91 <109462612+Dennois91@users.noreply.github.com>
2023-02-23 13:43:41 +0100

    Starting Examination. 5/5 AOC Completed 5/5 AOC Refactored 1/5 AOC TestCoverage
    Datastructures with recursive search function completed.

--- src/weeks_With_KOTLIN/examination/AOC2022_03/main.kt
+++ src/weeks_With_KOTLIN/examination/AOC2022_03/main.kt
@@ -8,6 +8,7 @@ var testList = mutableListOf<String>()
 fun main() {
     partA()
     partB()
+    improvedSolution()
 }
 
 fun partB() {
@@ -47,13 +48,27 @@ fun getCharValue(char: Set<Char>): Int {
     }
 }
 
+fun improvedSolution() {
+    val input = readFromFile()
+    val chunkedInput = input.chunked(3)
+
+    val points = chunkedInput.sumOf { lines ->
+        val commonChars = lines.map(String::toSet).reduce { a, b -> a.intersect(b) }
+        getCharValue(commonChars)
+    }
+    println(points)
+}
+
 fun readFromFile(): MutableList<String> {
     val filePath = Paths.get("src/weeks_With_KOTLIN/examination/AOC2022_03/data.txt")
     return Files.readAllLines(filePath)
 }
 /**
- * Implement chunk
+ * Återigen tittade jag på en lösning som jag inte tycker är bättre än min. Jag vet inte om personens lösning jag tittat
+ * på inte visste att man kan intersect fler än 2 stringar men jag tänker att jag ska pröva att använda mig av "chunk"
+ * för att testa på den iallafall. Inspo ifrån https://todd.ginsberg.com/post/advent-of-code/2022/day3/
+ *
+ * Nu efter att ha implementerat chunk så tar jag tillbaks min första åsikt. Denna nya lösning med chunk är faktiskt
+ * bättre än min egna lösning för partB och betydligt mer dynamisk om man tex skulle vilja ändra värdet på chunk size
  */
-
-
 //https://adventofcode.com/2022/day/3
